fca41419e1034c661c087f8382a53d109ca0c941
Craig Altenburg <craig@curuvar.com>
2025-05-26 20:12:20 -0400

    esp32c3 dispatch working

--- port/espressif/esp/src/hal/rtts-c3.zig
+++ port/espressif/esp/src/hal/rtts-c3.zig
@@ -63,14 +63,20 @@ pub fn configure(comptime RTTS: type, comptime config: RTTS.Configuration) type 
         /// Initialize the stack for a task as though it had been swapped out
         ///
         pub fn initialize_stack(in_stack: [*]usize, in_pc: *const fn () void) [*]usize {
-            var sp = in_stack - 32;
+            var sp = in_stack - 44;
 
-            for (sp[2..32]) |*reg| {
+            sp[0] = 0;
+            sp[1] = 0;
+            sp[2] = 0;
+            sp[3] = 0;
+
+            for (sp[4..34]) |*reg| {
                 reg.* = 0xface_fade; // so we can spot bad values
             }
 
-            sp[0] = @intFromPtr(in_pc);
-            sp[1] = if (config.run_unprivileged) 0x0000_0080 else 0x0000_1880;
+            sp[34] = @intFromPtr(in_stack);
+            sp[35] = @intFromPtr(in_pc);
+            sp[36] = if (config.run_unprivileged) 0x0000_0081 else 0x0000_1881;
 
             return sp;
         }
@@ -82,10 +88,16 @@ pub fn configure(comptime RTTS: type, comptime config: RTTS.Configuration) type 
 
             // Set up null task stack
 
-            null_task_stack_pointer = @ptrCast(&null_task_stack[null_task_stack_len - 32]);
+            null_task_stack_pointer = @ptrCast(&null_task_stack[null_task_stack_len - 44]);
 
-            null_task_stack[0] = @intFromPtr(&null_task_loop);
-            null_task_stack[1] = if (config.run_unprivileged) 0x0000_0080 else 0x0000_1880;
+            null_task_stack_pointer[0] = 0;
+            null_task_stack_pointer[1] = 0;
+            null_task_stack_pointer[2] = 0;
+            null_task_stack_pointer[3] = 0;
+
+            null_task_stack_pointer[34] = @intFromPtr(&null_task_stack) + 4 * null_task_stack_len;
+            null_task_stack_pointer[35] = @intFromPtr(&null_task_loop);
+            null_task_stack_pointer[36] = if (config.run_unprivileged) 0x0000_0081 else 0x0000_1881;
 
             // ### TODO ### uncomment below when we allow these to be set at runtime
             // _ = cpu.interrupt.core.set_handler(.Exception, .{ .riscv = machine_exception_ISR });
@@ -150,8 +162,8 @@ pub fn configure(comptime RTTS: type, comptime config: RTTS.Configuration) type 
                         // We found the task we want to run.  Get the initial PC from
                         // the initialized stack then clear the stack.
 
-                        target_pc = a_task.stack_pointer[0];
-                        target_sp = a_task.stack_pointer + 32;
+                        target_pc = a_task.stack_pointer[35];
+                        target_sp = a_task.stack_pointer + 44;
 
                         std.log.debug("Starting task {s} pc: 0x{x:08} sp: 0x{x:08}", .{ @tagName(a_task.tag), target_pc, @intFromPtr(target_sp) });
                         break;
@@ -162,32 +174,31 @@ pub fn configure(comptime RTTS: type, comptime config: RTTS.Configuration) type 
             if (config.run_unprivileged) {
                 asm volatile (
                     \\    mv    sp, %[sp]        // Set the process stack pointer
-                    \\    li    t0, 0x00000080   // Set the MSTATUS register
+                    \\    li    t0, 0x00000081   // Set the MSTATUS register
                     \\    csrs  MSTATUS, t0
                     \\    csrs  MEPC, %[pc]      // Set the MEPC register to the target PC
                     \\    mret
                     :
                     : [sp] "r" (target_sp),
-                    [pc] "r" (target_pc),
+                      [pc] "r" (target_pc),
                     : "t0"
                 );
             } else {
                 asm volatile (
                     \\    mv    sp, %[sp]        // Set the process stack pointer
-                    \\    li    t0, 0x00001880   // Set the MSTATUS register
+                    \\    li    t0, 0x00001881   // Set the MSTATUS register
                     \\    csrs  MSTATUS, t0
                     \\    csrs  MEPC, %[pc]      // Set the MEPC register to the target PC
                     \\    mret
                     :
                     : [sp] "r" (target_sp),
-                    [pc] "r" (target_pc),
+                      [pc] "r" (target_pc),
                     : "t0"
                 );
             }
 
             unreachable;
         }
-
 
         //==============================================================================
         // Interrupt service routines
@@ -201,180 +212,116 @@ pub fn configure(comptime RTTS: type, comptime config: RTTS.Configuration) type 
         /// This isr fires when machine exception is triggered.
         /// We use this to handle ecall instructions.
         ///
-        /// This ISR is registered as a naked function, so we can save the registers
-        /// and restore the registers in a predicable way. This function calls
-        /// "do_machine_exception" that does the actual dispatch in Zig.
-
-        // The riscv does not push anything when a trap happens.  After the save
-        // the stack (pointed to by sp) looks like this:
+        //     // Upon Entry the stack layout is:
         //
-       //      +------------+
-        //      |  MEPC      | <- Stack pointer
-        //      |  MSTATUS   | +    4  ( 1)
-        //      |  gp        | +    8  ( 2)
-        //      |  tp        | +   12  ( 3)
-        //      |  t6        | +   16  ( 4)
-        //      |  t5        | +   20  ( 5)
-        //      |  t4        | +   24  ( 6)
-        //      |  t3        | +   28  ( 7)
-        //      |  t2        | +   32  ( 8)
-        //      |  t1        | +   36  ( 9)
-        //      |  t0        | +   40  (10)
-        //      |  a7        | +   44  (11)
-        //      |  a6        | +   48  (12)
-        //      |  a5        | +   52  (13)
-        //      |  a4        | +   56  (14)
-        //      |  a3        | +   60  (15)
-        //      |  a2        | +   64  (16)
-        //      |  a1        | +   68  (17)
-        //      |  a0        | +   72  (18)
-        //      |  s11       | +   76  (19)
-        //      |  s10       | +   80  (20)
-        //      |  s9        | +   84  (21)
-        //      |  s8        | +   88  (22)
-        //      |  s7        | +   92  (23)
-        //      |  s6        | +   96  (24)
-        //      |  s5        | +  100  (25)
-        //      |  s4        | +  104  (26)
-        //      |  s3        | +  108  (27)
-        //      |  s2        | +  112  (28)
-        //      |  s1        | +  116  (29)
-        //      |  s0        | +  120  (30)
-        //      |  ra        | +  124  (31)
+        //      +------------+
+        //      |            | <--- Stack pointer
+        //      |            | +    4  ( 1)
+        //      |            | +    8  ( 2)
+        //      |            | +   12  ( 3)
+        //      |  ra        | +   16  ( 4) <--- TrapFrame pointer
+        //      |  t0        | +   20  ( 5)    +  4 ( 1)
+        //      |  t1        | +   24  ( 6)    +  8 ( 2)
+        //      |  t2        | +   28  ( 7)    + 12 ( 3)
+        //      |  t3        | +   32  ( 8)    + 16 ( 4)
+        //      |  t4        | +   36  ( 9)    + 20 ( 5)
+        //      |  t5        | +   40  (10)    + 24 ( 6)
+        //      |  t6        | +   44  (11)    + 28 ( 7)
+        //      |  a0        | +   48  (12)    + 32 ( 8)
+        //      |  a1        | +   52  (13)    + 36 ( 9)
+        //      |  a2        | +   56  (14)    + 40 (10)
+        //      |  a3        | +   60  (15)    + 44 (11)
+        //      |  a4        | +   64  (16)    + 48 (12)
+        //      |  a5        | +   68  (17)    + 52 (13)
+        //      |  a6        | +   72  (18)    + 56 (14)
+        //      |  a7        | +   76  (19)    + 60 (15)
+        //      |  s0        | +   80  (20)    + 64 (16)
+        //      |  s1        | +   84  (21)    + 68 (17)
+        //      |  s2        | +   88  (22)    + 72 (18)
+        //      |  s3        | +   92  (23)    + 76 (19)
+        //      |  s4        | +   96  (24)    + 80 (20)
+        //      |  s5        | +  100  (25)    + 84 (21)
+        //      |  s6        | +  104  (26)    + 88 (22)
+        //      |  s7        | +  108  (27)    + 92 (23)
+        //      |  s8        | +  112  (28)    + 96 (24)
+        //      |  s9        | +  116  (29)    +100 (25)
+        //      |  s10       | +  120  (30)    +104 (26)
+        //      |  s11       | +  124  (31)    +108 (27)
+        //      |  gp        | +  128  (32)    +112 (28)
+        //      |  tp        | +  132  (33)    +116 (29)
+        //      |  sp        | +  136  (34)    +120 (30)
+        //      |  pc        | +  140  (35)    +124 (31)
+        //      |  mstatus   | +  144  (36)    +128 (32)
+        //      |  mcause    | +  148  (37)    +132 (33)
+        //      |  mtval     | +  152  (38)    +136 (34)
+        //      |            | +  160  (40)    +140 (35)
+        //      |            | +  164  (41)    +144 (36)
+        //      |            | +  168  (42)    +148 (37)
+        //      |            | +  172  (43)    +152 (38)
         //      +------------+
 
-
-        pub fn machine_exception_ISR() callconv(.naked) noreturn {
+        fn _machine_exception_ISR(_: *cpu.TrapFrame) callconv(.naked) void {
             asm volatile (
-                \\     addi    sp, sp, -128
-                \\     sw      ra,  124(sp)
-                \\     sw      s0,  120(sp)
-                \\     sw      s1,  116(sp)
-                \\     sw      s2,  112(sp)
-                \\     sw      s3,  108(sp)
-                \\     sw      s4,  104(sp)
-                \\     sw      s5,  100(sp)
-                \\     sw      s6,  96(sp)
-                \\     sw      s7,  92(sp)
-                \\     sw      s8,  88(sp)
-                \\     sw      s9,  84(sp)
-                \\     sw      s10, 80(sp)
-                \\     sw      s11, 76(sp)
-                \\     sw      a0,  72(sp)
-                \\     sw      a1,  68(sp)
-                \\     sw      a2,  64(sp)
-                \\     sw      a3,  60(sp)
-                \\     sw      a4,  56(sp)
-                \\     sw      a5,  52(sp)
-                \\     sw      a6,  48(sp)
-                \\     sw      a7,  44(sp)
-                \\     sw      t0,  40(sp)
-                \\     sw      t1,  36(sp)
-                \\     sw      t2,  32(sp)
-                \\     sw      t3,  28(sp)
-                \\     sw      t4,  24(sp)
-                \\     sw      t5,  20(sp)
-                \\     sw      t6,  16(sp)
-                \\     sw      tp,  12(sp)
-                \\     sw      gp,  8(sp)
-                \\     csrr    ra,  MSTATUS
-                \\     sw      ra,  4(sp)
-                \\     csrr    ra,  MEPC
-                \\     sw      ra,  0(sp)
-                \\
-                \\     mv      a1, sp   // a0 already has the ecall code.
-                \\     call    %[meh]
-                \\     mv      sp, a0
-                \\
-                \\     lw      ra, 0(sp)
-                \\     csrw    MEPC, ra
-                \\     lw      ra, 4(sp)
-                \\     csrw    MSTATUS, ra
-                \\     lw      gp,  8(sp)
-                \\     lw      tp,  12(sp)
-                \\     lw      t6,  16(sp)
-                \\     lw      t5,  20(sp)
-                \\     lw      t4,  24(sp)
-                \\     lw      t3,  28(sp)
-                \\     lw      t2,  32(sp)
-                \\     lw      t1,  36(sp)
-                \\     lw      t0,  40(sp)
-                \\     lw      a7,  44(sp)
-                \\     lw      a6,  48(sp)
-                \\     lw      a5,  52(sp)
-                \\     lw      a4,  56(sp)
-                \\     lw      a3,  60(sp)
-                \\     lw      a2,  64(sp)
-                \\     lw      a1,  68(sp)
-                \\     lw      a0,  72(sp)
-                \\     lw      s11, 76(sp)
-                \\     lw      s10, 80(sp)
-                \\     lw      s9,  84(sp)
-                \\     lw      s8,  88(sp)
-                \\     lw      s7,  92(sp)
-                \\     lw      s6,  96(sp)
-                \\     lw      s5,  100(sp)
-                \\     lw      s4,  104(sp)
-                \\     lw      s3,  108(sp)
-                \\     lw      s2,  112(sp)
-                \\     lw      s1,  116(sp)
-                \\     lw      s0,  120(sp)
-                \\     lw      ra,  124(sp)
-                \\     addi    sp,  sp, 128
-                \\     mret
+                \\    mv    a1, sp   // frame pointer alredy in a0
+                \\    mv    s0, ra
+                \\    call  %[meh]
+                \\    mv    sp, a0
+                \\    jr    s0
                 :
                 : [meh] "i" (do_machine_exception),
             );
         }
 
+        // We have to cheat a bit here.  This function above is callconv (.naked) because we need
+        // to be precise in our register usage, but the ISR is callconv(.c), so we need to cast it
+        // to a callconv(.c) InterruptHandler function pointer.
+
+        pub const machine_exception_ISR: cpu.InterruptHandler = @ptrCast(&_machine_exception_ISR);
+
         /// This function is called from the machine_exception_ISR
         ///
         /// It does the actual dispatching of the ecall instruction
         ///
-        export fn do_machine_exception(in_code: SvcID, sp: [*]usize) callconv(.c) [*]usize {
+        export fn do_machine_exception(in_frame: *cpu.TrapFrame, sp: [*]usize) callconv(.c) [*]usize {
+            var new_sp: [*]usize = sp;
 
-            std.log.debug("MEH: in_code: 0x{x:02} sp: 0x{x:08}", .{ @intFromEnum(in_code), @intFromPtr(sp) });
+            //           std.log.debug("MEH: frame: 0x{x:08} sp: 0x{x:08} pc: 0x{x:08}", .{ @intFromPtr(in_frame), @intFromPtr(sp), in_frame.pc });
 
-            var ret_sp: [*]usize = sp;
-
-            const cause = cpu.csr.mcause.read().code;
-
-            if (cause == 0x08 or cause == 0x0b) {
+            if (in_frame.mcause == 0x08 or in_frame.mcause == 0x0b) {
                 // We got here due to an ecall instruction.  The what that works
                 // leaves the PC pointing to the ecall instruction itself.  We
                 // must bump it by four bytes to point to the next instruction.
 
-                sp[0] += 4;
+                const in_code: SvcID = @enumFromInt(in_frame.a0);
+
+                in_frame.pc += 4;
 
                 // Dispatch the instruction. This may change the stack pointer.
                 // if a new task is dispatched.
 
                 switch (in_code) {
-                    .yield =>
-                    {
-                        std.log.debug("MEH: yield", .{});
+                    .yield => {
+//                        std.log.debug("MEH: yield", .{});
                         if (RTTS.current_task[0]) |task| {
                             task.state = .yielded;
-                            ret_sp = RTTS.find_next_task_sp(sp);
+                            new_sp = RTTS.find_next_task_sp(sp);
                         }
                     },
-                    .significant_event =>
-                    {
-                        std.log.debug("MEH: significant_event", .{});
+                    .significant_event => {
+//                        std.log.debug("MEH: significant_event", .{});
                         for (&RTTS.sig_event) |*sig_event| {
                             sig_event.* = true;
                         }
-                        ret_sp = RTTS.find_next_task_sp(sp);
+                        new_sp = RTTS.find_next_task_sp(sp);
                     },
-                    .wait =>
-                    {
-                        std.log.debug("MEH: wait", .{});
+                    .wait => {
+//                        std.log.debug("MEH: wait", .{});
                         if (RTTS.current_task[0]) |task| {
                             // We need to wait if we have a mask and no
                             // mask bit has a corresponding event flag set.
                             if (task.event_mask != 0 and task.event_mask & task.event_flags == 0) {
                                 task.state = .waiting;
-                                ret_sp = RTTS.find_next_task_sp(sp);
+                                new_sp = RTTS.find_next_task_sp(sp);
                             }
                         }
                     },
@@ -384,7 +331,11 @@ pub fn configure(comptime RTTS: type, comptime config: RTTS.Configuration) type 
                 }
             }
 
-            return ret_sp;
+            //            for (0..44) |i| {
+            //                std.log.debug("New: sp[{d:2}] frame[{d:2}]: 0x{x:08}", .{ i, if (i < 4) 0 else i-4, new_sp[i] });
+            //            }
+
+            return new_sp;
         }
 
         //==============================================================================
